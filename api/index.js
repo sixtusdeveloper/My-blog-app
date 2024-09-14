b5b41a1b15cf36b57bfa522b37f66e963faa01c6
sixtusdeveloper <sixtusushrey@gmail.com>
2024-09-14 13:45:56 +0100

    commit on creating and connecting Mongodb successfully

--- api/index.js
+++ api/index.js
@@ -1,4 +1,17 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+mongoose
+.connect(process.env.MONGO)
+.then(() => { console.log('Successfully Connected to MongoDB!');
+
+})
+.catch(err => { console.log(err); 
+    
+});
 
 const app = express();
 
