7540569db6078fdb6c7b81b1a4e7b4a6c3937aba
sixtusdeveloper <sixtusushrey@gmail.com>
2024-09-14 09:29:28 +0100

    commit on Header component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,11 +5,13 @@ import SignIn from "./pages/Signin";
 import SignUp from "./pages/SignUp";
 import Dashboard from "./pages/Dashboard";
 import Projects from "./pages/Projects";
+import Header from "./components/Header";
 
 
 export default function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
@@ -17,8 +19,6 @@ export default function App() {
         <Route path="/sign-up" element={<SignUp />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/projects" element={<Projects />} />
-
-
       </Routes>
     
     </BrowserRouter>
