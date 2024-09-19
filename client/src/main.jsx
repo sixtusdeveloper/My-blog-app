0d34e03316822ccca67b4f3b49def92ae6aca244
sixtusdeveloper <sixtusushrey@gmail.com>
2024-09-19 18:37:27 +0100

    implemented redux-persist funtionality

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,13 +1,15 @@
-// import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import ReactDOM from 'react-dom/client';
 import './index.css'
-import { store } from './redux/store.js'  // Import the store
+import { store, persistor } from './redux/store.js'  // Import the store
+import { PersistGate } from 'redux-persist/integration/react'  // Import the PersistGate component  
 import { Provider } from 'react-redux'  // Import the Provider component  
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+  <PersistGate persistor={persistor}>
+    <Provider store={store}>
+      <App />
+    </Provider>,
+  </PersistGate>
 );
