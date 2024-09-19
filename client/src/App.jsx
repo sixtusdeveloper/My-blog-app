3c7362f7002b4fcd55bb7f4f769d24311b3e6ac7
sixtusdeveloper <sixtusushrey@gmail.com>
2024-09-19 17:53:39 +0100

    commit on Redux installation and implementation

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route, createBrowserRouter } from "react-router-dom"
 import Home from "./pages/Home";
 import About from "./pages/About";
-import SignIn from "./pages/Signin";
+import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import Dashboard from "./pages/Dashboard";
 import Projects from "./pages/Projects";
--- client/vite.config.js
+++ client/vite.config.js
@@ -1,5 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill';
+import { NodeModulesPolyfillPlugin } from '@esbuild-plugins/node-modules-polyfill';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,4 +14,27 @@ export default defineConfig({
     },
   },
   plugins: [react()],
+  resolve: {
+    alias: {
+      // Ensuring the polyfill is used for Buffer
+      buffer: 'buffer',
+    },
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      // Defining globalThis to support global variables
+      define: {
+        global: 'globalThis',
+      },
+      plugins: [
+        // Polyfills for Node.js globals and modules
+        NodeGlobalsPolyfillPlugin({
+          process: true,
+          buffer: true, // Polyfill Buffer
+        }),
+        NodeModulesPolyfillPlugin(),
+      ],
+    },
+  },
 });
+
