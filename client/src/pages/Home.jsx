c7cb29800835c94142bb806e2cbc409e45eeccbf
sixtusdeveloper <sixtusushrey@gmail.com>
2024-09-21 09:59:42 +0100

    completed Sidebar for the Dashboard

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,9 +3,7 @@ import React from 'react'
 const Home = () => {
   return (
     <div className='min-h-screen bg-white dark:bg-[rgb(16,23,42)] w-full pt-20'>
-      
-      Home
-      
+      Home 
     </div>
   )
 }
