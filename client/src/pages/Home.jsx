eb24944380703a5346050463e137bb5864820176
sixtusdeveloper <sixtusushrey@gmail.com>
2024-09-21 02:27:58 +0100

    commit layout adjustment

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div className='min-h-screen py-20'>Home</div>
+    <div className='min-h-screen bg-white dark:bg-[rgb(16,23,42)] w-full pt-20'>
+      
+      Home
+      
+    </div>
   )
 }
 
