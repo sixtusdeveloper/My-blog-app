4b1472a5f56406ce87f1ec13be0f75d510eb5500
sixtusdeveloper <sixtusushrey@gmail.com>
2024-09-28 21:15:24 +0100

    created a post API route

--- api/controllers/post.controller.js
+++ api/controllers/post.controller.js
@@ -2,14 +2,15 @@ import { errorHandler } from "../utils/error.js"
 import Post from '../models/post.model.js'
 
 export const create = async (req, res, next) => {
-    if(!req.body.isAdmin){
+   
+    if(!req.user.isAdmin){
         return next(errorHandler('You are not allowed to create a post', 403));
     }
 
     if(!req.body.title || !req.body.content){
         return next(errorHandler('Title and content are required', 400));   
     }
-    const slug = req.body.title.split(' ').join('-').toLowerCase().replace(/[^a-zA-Z0-9-]/g, '-');
+    const slug = req.body.title.split(' ').join('-').toLowerCase().replace(/[^a-zA-Z0-9-]/g, '');
 
     const newPost = new Post({
         ...req.body, 
--- api/models/post.model.js
+++ api/models/post.model.js
@@ -1,44 +1,43 @@
 import mongoose from "mongoose";
 
 const postSchema = new mongoose.Schema(
-    {
-        userId: {
-        type: String,
-        required: true,
-       },
+  {
+    userId: {
+      type: String,
+      required: true,
+    },
 
-       content: {
-        type: String,
-        required: true,
-       },
+    content: {
+      type: String,
+      required: true,
+    },
 
-       title: {
-        title: String,
-        required: true,
-        unique: true,
-       },
+    title: {
+      type: String, // Corrected key
+      required: true,
+      unique: true,
+    },
 
-       image: {
-         type: String,
-         default: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSxR_rp3nHPV73AXGPcOVFX8v8wCh2qw2QiEQ&s',
-       },
+    image: {
+      type: String,
+      default:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSxR_rp3nHPV73AXGPcOVFX8v8wCh2qw2QiEQ&s",
+    },
 
-       category: {
-         type: String,
-         default: 'uncategorized',
-       },
+    category: {
+      type: String,
+      default: "uncategorized",
+    },
 
-       slug: {
-        type: String,
-        required: true,
-        unique: true,
-       }
+    slug: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+  },
+  { timestamps: true }
+);
 
-    }, { timestamps: true }
-)
-
-
-
-const Post = mongoose.model('Post', postSchema)
+const Post = mongoose.model("Post", postSchema);
 
 export default Post;
