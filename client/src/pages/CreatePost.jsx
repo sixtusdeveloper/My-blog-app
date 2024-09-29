60bd9ace05acca4601b700e3db06014072f2417a
sixtusdeveloper <sixtusushrey@gmail.com>
2024-09-29 14:38:36 +0100

    completed create post functionality

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -8,13 +8,15 @@ import { useState } from 'react';
 import { CircularProgressbar } from 'react-circular-progressbar';
 import 'react-circular-progressbar/dist/styles.css';
 
+
 export default function CreatePost() {
+    
     const [file, setFile] = useState(null);
     const [imageUploadProgress, setImageUploadProgress] = useState(null);
     const [imageUploadError, setImageUploadError] = useState(null); 
     const navigate = useNavigate(); 
     const [formData, setFormData] = useState({});   
-
+    const [publishError, setPublishError] = useState(null);    
     const hangleUploadImage = async () => {
 
         try {
@@ -51,16 +53,48 @@ export default function CreatePost() {
         
     }
 
+    const handleSubmit = async (e) => { 
+        e.preventDefault();
+        try {
+            const res = await fetch('/api/post/create', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(formData),
+            });
+            const data = await res.json();
+            if(!res.ok){    
+               setPublishError(data.message);
+               return;
+            }
+
+            if(res.ok){
+                setPublishError(null);   
+                navigate(`/post/${data.slug}`);  
+            }
+        }
+        catch (error) {
+            setPublishError('Post creation failed. Please try again');
+        }
+    }
+
     return (
         <div className='bg-white dark:bg-[rgb(16,23,42)] min-h-screen px-4 md:px-10 w-full'>
             <div className="max-w-3xl py-20 mx-auto">
                 <h1 className='text-center font-semibold text-3xl my-16 md:mt-20'>Create Your Post</h1>
 
-                <form className='flex flex-col gap-4'>
+                <form className='flex flex-col gap-4' onSubmit={handleSubmit}>
+                     
                     <div className='flex flex-col gap-4 sm:flex-row justify-between'>
-                        <TextInput type='text' placeholder='Title' required id='title' className='flex-1' />
+                        <TextInput type='text' placeholder='Title' required id='title' className='flex-1' onChange={(e) => setFormData({
+                            ...formData, title: e.target.value  
+                        })}/>
 
-                        <Select>
+                        <Select onChange={(e) => setFormData({
+                            ...formData, category: e.target.value 
+                           })}
+                         >
                             <option value='uncategorized'>Select a Category</option>
                             <option value='javascript'>JavaScript</option>
                             <option value='reactjs'>React.js</option>
@@ -95,8 +129,15 @@ export default function CreatePost() {
                     )}  
                     
                     {/* The Editor */}
-                    <ReactQuill theme='snow' placeholder='Write something amazing...' className='h-72 mb-12' required />    
+                    <ReactQuill theme='snow' placeholder='Write something amazing...' 
+                    className='h-72 mb-12' 
+                    required 
+                    onChange={(value) => setFormData({
+                        ...formData, content: value
+                    })} 
+                    />    
                     <Button type='submit' gradientDuoTone='purpleToPink'>PUBLISH POST</Button>
+                    {publishError && <Alert color='failure' className='my-2' type='danger'>{publishError}</Alert>} 
                 </form>
             
             </div>
