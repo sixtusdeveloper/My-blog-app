00e579890bf2def9269d1ccd7da067dcf8b6c621
sixtusdeveloper <sixtusushrey@gmail.com>
2024-10-15 15:10:28 +0100

    initiated a deploy process

--- api/index.js
+++ api/index.js
@@ -1,65 +1,3 @@
-// import express from 'express';
-// import mongoose from 'mongoose';
-// import dotenv from 'dotenv';
-// import cookieParser from 'cookie-parser';
-
-// import userRoutes from './routes/user.route.js';
-// import authRoutes from './routes/auth.route.js';
-// import postRoutes from './routes/post.route.js';
-// import commentRoutes from './routes/comment.route.js';  
-// import newsletterRoutes from './routes/newsletter.route.js';
-// import pollVoteRoutes from './routes/pollvote.route.js';
-// import contactRoutes from './routes/contact.route.js'; // Import the contact route
-
-// dotenv.config();  // Load environment variables
-
-// // Connect to MongoDB
-// mongoose
-//   .connect(process.env.MONGO)
-//   .then(() => {
-//     console.log('Successfully Connected to MongoDB!');
-//   })
-//   .catch(err => {
-//     console.log(err);
-//   });
-
-// const app = express();
-
-// // Middleware
-// app.use(express.json());  // Parse JSON request bodies
-// app.use(cookieParser());  // Parse cookie headers
-
-// // Routes
-// app.use('/api/user', userRoutes);
-// app.use('/api/auth', authRoutes);
-// app.use('/api/post', postRoutes);
-// app.use('/api/comment', commentRoutes);
-// app.use('/api/newsletter', newsletterRoutes);  // Newsletter subscription route
-// app.use('/api/pollVote', pollVoteRoutes);
-// app.use('/api/contact', contactRoutes); // Use the contact route
-
-// // Error Handling Middleware
-// app.use((err, req, res, next) => {
-//   const statusCode = err.statusCode || 500;
-//   const message = err.message || 'Internal Server Error';
-  
-//   res.status(statusCode).json({
-//     success: false,
-//     statusCode,
-//     message,
-//   });
-// });
-
-// // Start the server
-// app.listen(3000, () => {
-//   console.log('Server is running on port 3000!');
-// }); 
-
-
-
-
-
-
 import dotenv from 'dotenv';
 dotenv.config();
 
@@ -75,6 +13,7 @@ import commentRoutes from './routes/comment.route.js';
 import newsletterRoutes from './routes/newsletter.route.js';
 import pollVoteRoutes from './routes/pollvote.route.js';
 import contactRoutes from './routes/contact.route.js'; // Import the contact route
+import path from 'path';  // Import the path module
 
 
 // dotenv.config();  // Load environment variables
@@ -88,6 +27,8 @@ mongoose
   .catch(err => {
     console.log(err);
   });
+
+   const __dirname = path.resolve();  // Define the project root directory  
 
 const app = express();
 
@@ -103,6 +44,9 @@ app.use('/api/comment', commentRoutes);
 app.use('/api/newsletter', newsletterRoutes);  // Newsletter subscription route
 app.use('/api/pollVote', pollVoteRoutes);
 app.use('/api/contact', contactRoutes); // Use the contact route
+
+// Serve static assets in production
+app.use(express.static(path.join(__dirname, '/client/build'))); 
 
 
 // Error Handling Middleware
