a6df4bcd400a1e6eb0881b908ba672591c793589
sixtusdeveloper <sixtusushrey@gmail.com>
2024-11-21 19:02:48 +0100

    updated the post controller

--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -19,25 +19,5 @@ export default router;
 
 
 
-// import express from 'express';
-// import { verifyToken } from '../utils/verifyUser.js';
-// import authorize from '../utils/authorize.js';
-// import { create, getposts, deletepost, updatepost } from '../controllers/post.controller.js';
-
-// const router = express.Router();
-
-// router.post('/create', verifyToken, authorize(['author', 'admin']), create);
-// router.get('/getposts', getposts);
-// router.delete('/deletepost/:postId/:userId', verifyToken, authorize(['admin', 'author']), deletepost);
-// router.put('/updatepost/:postId/:userId', verifyToken, authorize(['admin', 'author']), updatepost);
-
-// export default router;
-
-
-
-
-
-
-
 
 
